05df670a6b3e63eb103afa39a7a036c1addec045
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-06-02 05:24:45 +0600

    static Image for idea svg

--- app/components/sections/Roadmap.tsx
+++ app/components/sections/Roadmap.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import HeaderText from "../TextHeader";
 import AconomyCard from "../../../public/images/Aconomy Card.svg";
+import Idea from "../../../public/images/Idea.svg";
 
 type SectionContent = {
   text: string;
@@ -20,31 +21,24 @@ const data: Data = {
   },
   Finance: {
     text: "Your finance content here...",
-    // No image for this section
   },
   Finances: {
     text: "Your finance content here...",
-    // No image for this section
   },
   Finansce: {
     text: "Your finance content here...",
-    // No image for this section
   },
   Finssansce: {
     text: "Your finance content here...",
-    // No image for this section
   },
   Finanssce: {
     text: "Your finance content here...",
-    // No image for this section
   },
   Finassnsce: {
     text: "Your finance content here...",
-    // No image for this section
   },
   Finassnscse: {
     text: "Your finance content here...",
-    // No image for this section
   },
   // Add more sections as needed
 };
@@ -54,20 +48,15 @@ const RoadmapSection = () => {
 
   return (
     <section className="relative">
-              <div className="left-circle">
-          
-          </div>
-  
+      <div className="left-circle"></div>
       <HeaderText text="Roadmap" />
       <div className="flex gap-12 md:gap-0">
-        <nav className="flex flex-col space-y-2   w-1/4">
+        <nav className="flex flex-col space-y-2 w-1/4">
           {Object.keys(data).map((section) => (
             <button
               key={section}
-              className={`px-3 py-3 rounded-md flex items-center text-gray-400 text-[16px]  text-left ${
-                activeSection === section
-                  ? "text-white font-bold text-lg"
-                  : "pl-10"
+              className={`px-3 py-3 rounded-md flex items-center text-gray-400 text-[16px] text-left ${
+                activeSection === section ? "text-white font-bold text-lg" : "pl-10"
               }`}
               onClick={() => setActiveSection(section)}
             >
@@ -79,11 +68,11 @@ const RoadmapSection = () => {
           ))}
         </nav>
         <div className="p-1 md:p-4 w-3/4">
-          <p className=" text-xl md:text-4xl mb-12">{data[activeSection].text}</p>
+          <p className="text-xl md:text-4xl mb-12">{data[activeSection].text}</p>
           {data[activeSection].image && (
             <div className="h-full">
               <Image
-                src={data[activeSection].image || " "}
+                src={Idea}
                 alt={activeSection}
                 className="mt-4"
                 width={500}
@@ -94,27 +83,23 @@ const RoadmapSection = () => {
           )}
         </div>
       </div>
-      <div className=" flex flex-col gap-12  text-white items-center  relative justify-center  mt-24 w-full">
+      <div className="flex flex-col gap-12 text-white items-center relative justify-center mt-24 w-full">
         <div className="max-w-4xl text-center">
-          <p className="text-orange-500 text-center my-8 ">ACONOMY CARD</p>
-          <h1 className="text-center text-3xl md:text-5xl mb-24 ">
-            Utilize cryptocurrency for <br /> transactions in over 46 million
-            stores.
+          <p className="text-orange-500 text-center my-8">ACONOMY CARD</p>
+          <h1 className="text-center text-3xl md:text-5xl mb-24">
+            Utilize cryptocurrency for <br /> transactions in over 46 million stores.
           </h1>
-          <div className="flex justify-center  px-24 pt-0 pb-12 !w-full">
-            <div className="center-circle">
-
-            </div>
-            <Image src={AconomyCard} alt="image" />
+          <div className="flex justify-center px-24 pt-0 pb-12 w-full">
+            <div className="center-circle"></div>
+            <Image src={AconomyCard} alt="Aconomy Card" />
           </div>
-          <div className="flex justify-center  items-center gap-6 lg:gap-24">
-          <p className=" pl-2 md:pl-5 border-l-2 border-orange-700 h-12 mt-6 flex items-center">No limits card</p>
-          <p className=" pl-2 md:pl-5 border-l-2 border-orange-700 h-12 mt-6 flex items-center">No limits card</p>
-          <p className=" pl-2 md:pl-5 border-l-2 border-orange-700 h-12 mt-6 flex items-center">No limits card</p>
+          <div className="flex justify-center items-center gap-6 lg:gap-24">
+            <p className="pl-2 md:pl-5 border-l-2 border-orange-700 h-12 mt-6 flex items-center">No limits card</p>
+            <p className="pl-2 md:pl-5 border-l-2 border-orange-700 h-12 mt-6 flex items-center">No limits card</p>
+            <p className="pl-2 md:pl-5 border-l-2 border-orange-700 h-12 mt-6 flex items-center">No limits card</p>
           </div>
         </div>
       </div>
-
     </section>
   );
 };
